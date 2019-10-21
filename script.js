6eff1d059f7eac88e1a6bcb261652e97baaf801a
ILVL <ihbindo@gmail.com>
2019-10-21 09:28:07 +0300

    alltofunc

--- script.js
+++ script.js
@@ -1,101 +1,68 @@
+let money, time;
 
-let money = prompt("Your monthly budget?"),
-    time = prompt("Enter Date YYYY-MM-DD");
+function start() {
 
-var appData = {
+    money = +prompt("Your monthly budget?","");
+    time = prompt("Enter Date YYYY-MM-DD","");
+
+
+    while(isNaN(money) || money == "" || money == null) {
+
+        money = +prompt("Your monthly budget?", '');
+
+    }
+
+
+}
+
+
+start();
+
+
+
+
+let appData = {
 
     budget: money,
     expenses: {},
     optionalExpenses: {},
     income: [],
     timeData: time,
-    savings: false
+    savings: true
 };
 
 
 // ◊≈–≈« ÷» À FOR
 
-
-for (let i = 0; i < 2; i++) {
-
-    let a = prompt("Mandatory spends", "");
-        b = prompt("How much it cost?", "");
-
-    if ((typeof (a)) === 'string' && (typeof (a)) != null && (typeof (b)) != null
-
-        && a != '' && b != '' && a.length < 50) {
-        console.log("done");
-        appData.expenses[a] = b;
-    } else { 
-    console.log ("bad result");
-    i--;
-    }
-
-};
+function chooseExpenses() {
 
 
+    for (let i = 0; i < 2; i++) {
 
+        let a = prompt("Mandatory spends", ""),
+            b = prompt("How much it cost?", "");
 
-// ◊≈–≈« ÷» À WHILE 
+        if ((typeof (a)) === 'string' && (typeof (a)) != null && (typeof (b)) != null
 
-/*
+            && a != '' && b != '' && a.length < 50) {
+            console.log("done");
+            appData.expenses[a] = b;
+        } else {
+            i = i - 1;
+        }
 
-let i = 0;
-while (i < 2) {
-
-    let a = prompt("Mandatory spends", "");
-    b = prompt("How much it cost?", "");
-
-    if ((typeof (a)) === 'string' && (typeof (a)) != null && (typeof (b)) != null
-
-        && a != '' && b != '' && a.length < 50) {
-        console.log("done");
-        appData.expenses[a] = b;
-    } else {
-        console.log("bad result");
-        i--;
     }
 
 
-    i++;
+
 }
 
 
-*/
-
-
-// ÷» À ◊≈–≈« DO WHILE
-
-/*
-
-let i = 0;
-
-do {
-
-    let a = prompt("Mandatory spends", "");
-    b = prompt("How much it cost?", "");
-
-    if ((typeof (a)) === 'string' && (typeof (a)) != null && (typeof (b)) != null
-
-        && a != '' && b != '' && a.length < 50) {
-        console.log("done");
-        appData.expenses[a] = b;
-    } else {
-        console.log("bad result");
-        i--;
-    }
-
-
-    i++;
-}
-
-while (i < 2);
-
-*/
+chooseExpenses();
 
 
 
-appData.moneyPerDay = appData.budget / 30;
+appData.moneyPerDay = (appData.budget / 30).toFixed(1);
 
 
 alert("Daily Budget = " + appData.moneyPerDay);
@@ -106,3 +73,23 @@ if (appData.moneyPerDay < 100) {
     console.log("Medium level of life");
 } else if (appData.moneyPerDay > 2000) { console.log("High level of life") }
 else { console.log("SomethingWrong") }
+
+
+
+
+
+function checkSavings() {
+
+    if (appData.savings == true) {
+
+        let save = +prompt("What is your deposit funds?"),
+            percent = +prompt("What percent?");
+
+        appData.monthIncome = save / 100 / 12 * percent;
+        alert("monthly income from your deposit: " + appData.monthIncome);
+    }
+
+}
+
+
+checkSavings();
